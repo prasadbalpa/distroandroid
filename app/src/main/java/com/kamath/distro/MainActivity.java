7be6471ead789b2ab9a3258e342bbe8039817c55
prasadbalpa <prasad.nala@gmail.com>
2017-01-25 22:56:16 +0530

    Initial commit

---
+++ app/src/main/java/com/kamath/distro/MainActivity.java
@@ -0,0 +1,194 @@
+package com.kamath.distro;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.EditText;
+import android.content.Intent;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String logtag = "TwoButtonApp";//for use as the tag when logging
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonStart = (Button)findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(startListener); // Register the onClick listener with the implementation above
+
+        Button buttonStop = (Button)findViewById(R.id.buttonStop);
+        buttonStop.setOnClickListener(stopListener); // Register the onClick listener with the implementation above
+    }
+
+    //Create an anonymous implementation of OnClickListener
+    private OnClickListener startListener = new OnClickListener() {
+        EditText eText;
+        public void onClick(View v) {
+            Log.d(logtag,"onClick() called - start button");
+            //Toast.makeText(this, "The Start button was clicked.", Toast.LENGTH_LONG).show();
+            Log.d(logtag,"onClick() ended - start button");
+            eText = (EditText) findViewById(R.id.edittext);
+            eText.clearFocus();
+            new SendPostRequest().execute();
+        }
+    };
+
+    // Create an anonymous implementation of OnClickListener
+    private OnClickListener stopListener = new OnClickListener() {
+        public void onClick(View v) {
+            Log.d(logtag,"onClick() called - stop button");
+            //Toast.makeText(this, "The Stop button was clicked.", Toast.LENGTH_LONG).show();
+            Log.d(logtag,"onClick() ended - stop button");
+
+            //Intent intent = new Intent(this, GridViewActivity.class);
+            //startActivity(intent);
+        }
+    };
+
+
+    @Override
+    protected void onStart() {//activity is started and visible to the user
+        Log.d(logtag,"onStart() called");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {//activity was resumed and is visible again
+        Log.d(logtag,"onResume() called");
+        super.onResume();
+
+    }
+    @Override
+    protected void onPause() { //device goes to sleep or another activity appears
+        Log.d(logtag,"onPause() called");//another activity is currently running (or user has pressed Home)
+        super.onPause();
+
+    }
+    @Override
+    protected void onStop() { //the activity is not visible anymore
+        Log.d(logtag,"onStop() called");
+        super.onStop();
+
+    }
+    @Override
+    protected void onDestroy() {//android has killed this activity
+        Log.d(logtag,"onDestroy() called");
+        super.onDestroy();
+    }
+
+    public class SendPostRequest extends AsyncTask<String, Void, String> {
+
+        protected void onPreExecute(){}
+
+        protected String doInBackground(String... arg0) {
+            EditText eText;
+            try {
+
+                URL url = new URL("http://10.0.2.2:28090/instrument"); // here is your URL path
+
+                JSONObject postDataParams = new JSONObject();
+                eText = (EditText) findViewById(R.id.edittext);
+                postDataParams.put("instrumentid", eText.getText());
+                postDataParams.put("customerid", "abc@gmail.com");
+                Log.e("params",postDataParams.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(postDataParams));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode=conn.getResponseCode();
+
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                    BufferedReader in=new BufferedReader(
+                            new InputStreamReader(
+                                    conn.getInputStream()));
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    return sb.toString();
+
+                }
+                else {
+                    return new String("false : "+responseCode);
+                }
+            }
+            catch(Exception e){
+                return new String("Exception: " + e.getMessage());
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getApplicationContext(), result,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+        }
+        return result.toString();
+    }
+}
